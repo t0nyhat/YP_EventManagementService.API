e08b189111513a28bdadb5b70ea6b8eaf23b2db0
Tony Hat <exkitter@gmail.com>
2026-05-17 21:12:13 +1000

    chore(sprint6): add EF Core Design and scaffold integration test project
    - add Microsoft.EntityFrameworkCore.Design to API project
    
    - add integration test project with Testcontainers/EF/Npgsql/xUnit deps
    
    - add project to solution and keep a smoke test for build validation

---
+++ tests/EventManagementService.API.IntegrationTests/SmokeTests.cs
@@ -0,0 +1,10 @@
+namespace EventManagementService.API.IntegrationTests;
+
+public class SmokeTests
+{
+    [Fact]
+    public void PlaceholderTest_ShouldPass()
+    {
+        Assert.True(true);
+    }
+}
