3983e96e30c14dce9d17000f9be2460739bdb92e
Tony Hat <exkitter@gmail.com>
2026-05-17 21:58:59 +1000

    feat(sprint6): add initial ef migration and switch startup to database migrate

--- src/EventManagementService.API/Program.cs
+++ src/EventManagementService.API/Program.cs
@@ -59,7 +59,7 @@ var app = builder.Build();
 using (var scope = app.Services.CreateScope())
 {
     var db = scope.ServiceProvider.GetRequiredService<AppDbContext>();
-    db.Database.EnsureCreated();
+    db.Database.Migrate();
 }
 
 // ========== HTTP Request Pipeline ==========
